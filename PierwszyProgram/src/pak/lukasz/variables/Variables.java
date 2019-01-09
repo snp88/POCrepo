7142ace4649faed1ca8ac74ba1c0bfc87b79939a
Łukasz <snp88tag@gmail.com>
2019-01-09 20:19:12 +0100

    zmienne

---
+++ PierwszyProgram/src/pak/lukasz/variables/Variables.java
@@ -0,0 +1,18 @@
+package pak.lukasz.variables;
+
+public class Variables {
+	public static void main(String[] args) {
+	
+		int x = 42 + 23;
+		byte b = 127 ;
+		short s = Short.MAX_VALUE;
+		System.out.println(x+b+s);
+		
+		long l =Long.MAX_VALUE;
+		System.out.println(l);
+		float f = 0.0f;
+		System.out.println(f);
+	String str = "dupa";
+	System.out.println(str);
+	 }
+}
