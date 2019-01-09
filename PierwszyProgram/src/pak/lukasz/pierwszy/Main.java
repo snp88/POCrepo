f833a1f89ec7db4bb15d02be9f28302723a8c733
Xiaomi <Xiaomi@192.168.0.241>
2019-01-09 19:28:15 +0100

    first commit

---
+++ PierwszyProgram/src/pak/lukasz/pierwszy/Main.java
@@ -0,0 +1,10 @@
+package pak.lukasz.pierwszy;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("helloWorld 1 2");
+	}
+
+}
