09ca9003dbbcfdbd91d41f35a56106b04b800d0a
snp88 <snp88tag@gmail.com>
2019-01-11 00:45:06 +0100

    Initial udemy

--- Projekty/drugi/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-    //zmieniona nazwa repo kolejna zmiana nazwy i jszcze jedna
-}
