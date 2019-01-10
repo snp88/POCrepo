a35bce474ee021c79aa0ad59f31493ff57462cc2
Łukasz <snp88tag@gmail.com>
2019-01-10 15:07:32 +0100

    qqq

---
+++ PierwszyProgram/src/pak/lukasz/petle/Petle.java
@@ -0,0 +1,38 @@
+package pak.lukasz.petle;
+
+
+public class Petle {
+
+	public static void main(String[] args) {
+		/// petla for licznik , warunek wyjscia, zwiekszanie licznika
+		// jezeli licznik zadeklarowany jest w deklaracji petli nie mozna go uzyc poza pêtl¹
+		int i;
+		for(i = 0; i< 1000; i++) {
+			
+			System.out.println("Nr# " + i);
+			
+			
+		}
+		
+		/* ----petla nieskoñczona------
+		 for (;;) {
+			System.out.println("cialo petli");
+		}
+		
+		*/
+		System.out.println(i);
+		System.out.println("NowaPetla");
+		// inny zapis petli for
+		int j = 20;
+		for(; j>=10; j--) {
+			System.out.println(j);
+		}
+		System.out.println("NowaPetla");
+		for(int l = 0; l<10; l++) {
+			for(int l1 = 10; l1<10; l1++) {
+				
+			}
+		}
+	
+	}
+}
