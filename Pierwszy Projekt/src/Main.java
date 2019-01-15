b83a77c60f3b73be95de0c38822f2b0da1fe26fc
snp88 <snp88tag@gmail.com>
2019-01-15 12:05:21 +0100

    test

--- Pierwszy Projekt/src/Main.java
+++ Pierwszy Projekt/src/Main.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Scanner odczyt = new Scanner(System.in); //obiekt do odebrania danych od użytkownik
+    String imie;
+        System.out.println("Wprowadz swoje imie: ");
+    imie = odczyt.nextLine();
+
+        System.out.println("Witaj "+imie); //wyświetlamy powitanie
+
+
+
+
+
+
+
+
     }
 }
