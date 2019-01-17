4c65eabbc8bf95e4e63ab5d22d2b314ca339d2f6
snp88 <snp88tag@gmail.com>
2019-01-17 16:36:22 +0100

    Final Version

---
+++ Pierwszy Projekt/src/Heart.java
@@ -0,0 +1,10 @@
+public class Heart {
+/*
+    String b = "ralala" ;
+
+public String metoda1 (){
+    return String b;
+}
+
+*/
+}
--- Pierwszy Projekt/src/Main.java
+++ Pierwszy Projekt/src/Main.java
@@ -1,45 +1,67 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //System.out.println("Hello World!");
         Scanner odczyt = new Scanner(System.in); //obiekt do odebrania danych od użytkownik
         var licznik = 0;
         var wyb = 0.0;
-
+        System.out.println("Jak barodz mnie kochasz");
         do {
+            System.out.println("|||||||||||||||||||");
+            //System.out.println("licznik Petli " + licznik);
 
+            System.out.println("Wprowadz Liczbe od 1 do 10");
+            System.out.println("|||||||||||||||||||");
+            int sucess = 0;
 
-            System.out.println("-------------");
-            System.out.println("licznik Petli " + licznik);
-            System.out.println("Wprowadz Liczbę od 1 do 10");
-            wyb = odczyt.nextLong();
-            System.out.println("Wprowadzona Liczba");
-            System.out.println(wyb);
+            do {
+                try {
+                    wyb = odczyt.nextLong();
+                    //System.out.println("Wprowadzona Liczba");
+                    //System.out.println(wyb);
+                    sucess = 1;
+                } catch (InputMismatchException e) {
+                    System.out.println("to nie jest liczba");
+                    System.out.println("wpisz liczbe jeszcze raz");
+                    odczyt.next( ); //zapobiega nieskonczonej petli po zlapaniu erroru
+                }
+
+            } while (sucess != 1);
+
             licznik++;
+            System.out.println("<------------->");
             if (wyb >= 0 && wyb < 3) {
                 System.out.println("słabiutko");
-            }
-            else if (wyb >= 3 && wyb < 6) {
+                System.out.println("możes próbujesz jeszcze raz");
+                licznik =0;
+            } else if (wyb >= 3 && wyb < 6) {
                 System.out.println("Myslałem ze bedzie lepiej");
-            }
-            else if (wyb >= 6 && wyb < 8) {
+                System.out.println("możes próbujesz jeszcze raz");
+                licznik =0;
+            } else if (wyb >= 6 && wyb < 8) {
                 System.out.println("Tak jest bejbe :D");
-            }
-            else if (wyb >= 8 && wyb < 9) {
+                System.out.println("Juz prawie to mamy");
+                licznik =0;
+            } else if (wyb >= 8 && wyb < 9) {
                 System.out.println("To rozumiem :*");
-            }
-            else if (wyb >= 9 && wyb <= 10) {
+                licznik =10;
+            } else if (wyb >= 9 && wyb <= 10) {
                 System.out.println("Ja ciebie też");
-            }
-            else if (wyb < 1 || wyb > 10) {
-                System.out.println("liczba poza zakresem");
+                System.out.println("Sto razy mocniej");
+                licznik =10;
+            } else if (wyb < 1 || wyb > 10) {
+                System.out.println("liczba poza zakresem doceniam entuzjazm :D:D");
                 System.out.println("spróbuj jeszcze raz");
                 licznik = 0;
             }
+            System.out.println("<------------->");
         }
         while (licznik < 10);
+        System.out.println("||||||||||||||||||||||||||");
+        System.out.println("W końcu sie dogadalismy :D");
     }
 }
